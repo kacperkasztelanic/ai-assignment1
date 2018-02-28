fbae625b36e31c9121b9ce2ab1eed63197c581c5
Kacper <kcpr51@hotmail.com>
2018-02-28 16:44:16 +0100

    DataLoader finished

--- data_loader.py
+++ data_loader.py
@@ -1,9 +1,12 @@
 import numpy as np
 
+SOURCE_DIR = 'data'
+SOURCE_EXT = 'dat'
+SOLUTION_DIR = 'solutions'
+SOLUTION_EXT = 'sln'
 
-def load(filename: str):
-
-    path = 'data/' + filename
+def load_source(filename: str):
+    path = SOURCE_DIR + '/' + filename + '.' + SOURCE_EXT
     data_file = open(path, 'r')
     n = int(data_file.readline())
     data_file.readline()
@@ -18,3 +21,10 @@ def load(filename: str):
     matrix_distance = np.fromstring(matrix, dtype=int, sep=' ').reshape(n, n)
     return n, matrix_flow, matrix_distance
 
+
+def load_results(filename: str):
+    path = SOLUTION_DIR + '/' + filename + '.' + SOLUTION_EXT
+    data_file = open(path, 'r')
+    n, cost = data_file.readline().split()
+    vector = np.fromstring(data_file.readline(), dtype=int, sep=' ')
+    return int(n), int(cost), vector
